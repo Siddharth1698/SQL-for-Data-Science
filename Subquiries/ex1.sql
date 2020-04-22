12b314b812ff2c448a04dac5ccc6fabdcfa512ab
Siddharth M <siddharth.m98@gmail.com>
2020-04-22 22:40:01 +0530

    Subqueries

---
+++ Subquiries/ex1.sql
@@ -0,0 +1,9 @@
+SELECT AVG(standard_qty) avg_std, AVG(gloss_qty) avg_gls, AVG(poster_qty) avg_pst
+FROM orders
+WHERE DATE_TRUNC('month', occurred_at) = 
+     (SELECT DATE_TRUNC('month', MIN(occurred_at)) FROM orders);
+
+SELECT SUM(total_amt_usd)
+FROM orders
+WHERE DATE_TRUNC('month', occurred_at) = 
+      (SELECT DATE_TRUNC('month', MIN(occurred_at)) FROM orders);
