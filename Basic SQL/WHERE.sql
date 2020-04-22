0cf6acbed15c1e92794652f81f2aaf2e2fe918a0
Siddharth M <siddharth.m98@gmail.com>
2020-04-22 11:13:47 +0530

    Where

---
+++ Basic SQL/WHERE.sql
@@ -0,0 +1,12 @@
+1.Pulls the first 5 rows and all columns from the orders table that have a dollar amount of gloss_amt_usd greater than or equal to 1000.
+2.Pulls the first 10 rows and all columns from the orders table that have a total_amt_usd less than 500.
+
+
+1.SELECT *
+FROM orders
+WHERE gloss_amt_usd >= 1000
+LIMIT 5;
+2.SELECT *
+FROM orders
+WHERE total_amt_usd < 500
+LIMIT 10;
